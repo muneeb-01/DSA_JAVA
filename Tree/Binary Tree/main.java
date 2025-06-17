553d72d972d77be676a9a81382b9001317e6c2e0
muneeb-01 <muneerahmed8556@gmail.com>
2025-06-18 00:12:19 +0500

    commit

--- Tree/Binary Tree/main.java
+++ Tree/Binary Tree/main.java
@@ -3,15 +3,24 @@ import java.util.Queue;
 
 public class main {
     public static void main(String[] args) {
-        BT tree = new BT();
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
-        System.out.println(tree.isSymmetric());
+        BST bst = new BST();
+        bst.insert(10);
+        bst.insert(9);
+        bst.insert(8);
+        bst.insert(7);
+        bst.insert(6);
+        bst.insert(5);
+        bst.insert(4);
+        bst.insert(12);
+        bst.insert(18);
+        bst.insert(23);
+        bst.insert(21);
+        bst.insert(51);
+        bst.insert(15);
+
+        System.out.println(bst.isBinarySearchTree());
     }
 }
-
-class BT{
     class Node{
         int data;
         Node left;
@@ -23,8 +32,8 @@ class BT{
         }
     }
 
+class BT{
     Node root;
-
     void insert(int data){
         if(root == null){
             root = new Node(data);
@@ -49,6 +58,33 @@ class BT{
             }
         }
     }
+    boolean isBalance(){
+        return height(root) != -1;
+    }
+    void inOrder(Node root){
+        if(root == null) return;
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+    private int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        if(leftHeight == -1){
+            return -1;
+        }
+        int rightHeight = height(root.right);
+        if(rightHeight == -1){
+            return -1;
+        }
+        if(Math.abs(leftHeight - rightHeight) > 1){
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
     boolean isSymmetric(){
         return root == null || ismirror(root.left, root.right);
     }
@@ -61,4 +97,58 @@ class BT{
         }
         return (left.data == right.data) && ismirror(left.left, right.right) && ismirror(right.left, left.right);
     }
+    boolean isBinarySearchTree(){
+        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    boolean isBST(Node root, Integer min, Integer max){
+        if(root == null) return true;
+        if(root.data > min && root.data < max){
+            return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+        }
+        return false;
+    }
+}
+
+class BST extends BT{
+    @Override
+    void insert(int data){
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node root, int data) {
+        if(root == null){
+            return new Node(data);
+        }
+        if(data < root.data){
+            root.left = insertRec(root.left, data);
+        }
+        else if(data > root.data){
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    Integer get_max(){
+        if(root == null){
+            System.out.println("Tree is empty");
+            return null;
+        }
+        Node temp = root;
+        while(temp.right != null){
+            temp = temp.right;
+        }
+        return temp.data;
+    }
+    Integer get_min(){
+        if(root == null){
+            System.out.println("Tree is empty");
+            return null;
+        }
+        Node temp = root;
+        while(temp.left != null){
+            temp = temp.left;
+        }
+        return temp.data;
+    }
+
 }
